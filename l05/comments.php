05d73cfcdaeaabdc934dd5f926a4fc78ca101d37
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2021-02-08 20:03:57 +0200

    L05: Added comments examples

---
+++ l05/comments.php
@@ -0,0 +1,21 @@
+<?php
+
+// echo 'test 01', PHP_EOL;
+# echo 'test 02', PHP_EOL;
+/*echo 'test 03', PHP_EOL;*/
+/*
+echo 'test 04', PHP_EOL;
+echo 'test 05', PHP_EOL;
+*/
+echo 'test 06', PHP_EOL;
+
+/**
+ * Shows an example of API-style comment
+ * @param string|array $testA
+ * @param int $testB
+ * @return string
+ */
+function testSMTH($testA, $testB)
+{
+    return 'TEST';
+}
