0e08243348da32967085280a14909296d28c2eee
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2021-02-08 21:10:41 +0200

    L05: Added user interact console script

---
+++ l05/user_interact.php
@@ -0,0 +1,7 @@
+<?php
+
+$name = readline('Enter your name: ');
+$password = readline('Enter your password: ');
+$age = readline('Enter your age: ');
+
+echo "Hello {$name}, {$age} year old. Don't forgot your password: {$password}", PHP_EOL;
