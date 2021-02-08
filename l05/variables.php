15c9beb50af434000cca6cd332065730f469f429
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2021-02-08 20:46:04 +0200

    L05: Added variables examples

---
+++ l05/variables.php
@@ -0,0 +1,25 @@
+<?php
+
+$fruit = 'Orange';
+
+echo $fruit, PHP_EOL;
+echo $fruit, PHP_EOL;
+
+$fruit = 'Apple';
+echo $fruit, PHP_EOL;
+echo $fruit, PHP_EOL;
+
+// FAILS
+// test = 123; $ is required
+// $1test = 123; no numbers on start
+// $te^st = 123; no special characters
+// $te st = 123; no spaces
+
+// AVAILABLE
+$t1weq = 213;
+$_te_st = 123;
+
+$all_students_list = [];
+
+$allStudentsList = [];
+$minSum = 12;
