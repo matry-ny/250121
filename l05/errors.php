cf37cd161ebdf7c421550fa6d4bd576324b12505
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2021-02-09 12:02:40 +0200

    L05: Added errors logging

--- l05/errors.php
+++ l05/errors.php
@@ -1,10 +1,12 @@
 <?php
 
-//error_reporting(E_ALL ^ E_WARNING ^ E_ERROR);
-error_reporting(0);
+error_reporting(E_ALL);
+//error_reporting(0);
 
 $array = [];
 
-echo $t;
-var_dump($array['t']);
-var_dump(1 / 0);
+echo 1 + '2';
+
+//echo $t;
+//var_dump($array['t']);
+//var_dump(1 / 0);
