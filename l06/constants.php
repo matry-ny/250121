94f200dfd100a736ee78076383798f2f59572c77
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2021-02-11 20:02:28 +0200

    L06: Added constants demonstration

---
+++ l06/constants.php
@@ -0,0 +1,17 @@
+<?php
+
+define('APP_VERSION', '3.0.1');
+//define('APP_VERSION', '3.0.2');
+//APP_VERSION = '3.0.3';
+
+echo APP_VERSION, PHP_EOL;
+
+const POWER = '220V';
+//const POWER = '380V';
+
+echo POWER, PHP_EOL;
+
+const MY_COEFFICIENT = 4;
+$salary = 2000;
+echo $salary * MY_COEFFICIENT, PHP_EOL;
+
