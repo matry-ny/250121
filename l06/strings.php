9bf0a4926ba3913de9eed1dcf094c1157decf1dc
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2021-02-11 19:41:44 +0200

    L06: Added string formats demonstration

--- l06/strings.php
+++ l06/strings.php
@@ -10,4 +10,21 @@ print PHP_EOL;
 $g1 = print 'test 123';
 var_dump($g1);
 
+$name = 'Dmytro';
+echo 'Hello, user with name \'$name\'\n', PHP_EOL;
+echo "Hello, user with name \"{$name}\"\n";
 
+$htmlId = 'test';
+$nowdoc = <<<'HTML'
+<script type="text/javascript">
+    document.getElementById('#{$htmlId}').innerHTML;
+</script>
+HTML;
+echo $nowdoc, PHP_EOL;
+
+$heredoc = <<<HTML
+<script type="text/javascript">
+    document.getElementById('#{$htmlId}').innerHTML;
+</script>
+HTML;
+echo $heredoc, PHP_EOL;
