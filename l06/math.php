fa1bcf20ee3bbafbab6577701b00f8f28999442c
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2021-02-11 20:26:37 +0200

    L06: Added math operators demonstration

---
+++ l06/math.php
@@ -0,0 +1,23 @@
+<?php
+
+echo 1 + 2, PHP_EOL;
+echo 1 * 2, PHP_EOL;
+echo 1 - 2, PHP_EOL;
+echo 1 / 2, PHP_EOL;
+echo 543 * 0.2, PHP_EOL; // 20%
+
+echo 3 % 2, PHP_EOL;
+echo 3 % 5, PHP_EOL;
+echo 8 % 3, PHP_EOL;
+
+echo 3 ** 2, PHP_EOL;
+echo 3 ** 5, PHP_EOL;
+
+$result = 0;
+$result += 2;
+$result += 5;
+$result *= 2;
+$result /= 5;
+echo $result, PHP_EOL;
+
+
