222d6bfe4d1dabc0d59ebc0338968b36762849ee
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2021-02-15 21:43:30 +0200

    L07: Added homework

--- l07/conditions.php
+++ l07/conditions.php
@@ -35,3 +35,19 @@ echo PHP_EOL;
 
 echo $age == 33 ? 'Jesus' : ($age >= 18 ? 'OK' : 'FAIL');
 echo PHP_EOL;
+
+// Homework
+// 1 - print 123
+/*
+$x = ;
+if ($x == 1) {
+    echo 1;
+}
+if ($x == 2) {
+    echo 2;
+}
+if ($x == 3) {
+    echo 3;
+}
+*/
+// 2 - calculator
